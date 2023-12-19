9dee9079a181a998379bb67684d12df645b77d9a
Shaun Levy <sly@ndonline.ac.uk>
2023-12-19 11:03:32 +0000

    Added basic API functionality Shows entire JSON file in textblock

--- Health Advice Group/Menu.xaml.cs
+++ Health Advice Group/Menu.xaml.cs
@@ -11,6 +11,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Text.Json.Nodes;
+using Org.BouncyCastle.Asn1.Ocsp;
+using System.Net.Http;
 
 namespace Health_Advice_Group
 {
@@ -19,11 +22,29 @@ namespace Health_Advice_Group
     /// </summary>
     public partial class Menu : Window
     {
-        string requestCurrent = "http://api.weatherapi.com/v1/current.json?key=APIKEY&q=Leeds&aqi=no";
+        string baseRequestCurrent = "http://api.weatherapi.com/v1/current.json?key=APIKEY&q=LOCATION&aqi=no";
+        string requestCurrent;
         public Menu()
         {
             InitializeComponent();
+            requestCurrent = baseRequestCurrent.Replace("LOCATION", "Leeds");
             requestCurrent = requestCurrent.Replace("APIKEY", API.Key);
+            showJSON(requestCurrent);
+        }
+
+        private void btnUpdateLocation_Click(object sender, RoutedEventArgs e)
+        {
+            requestCurrent = baseRequestCurrent.Replace("LOCATION",txtLocation.Text);
+            MessageBox.Show(requestCurrent);
+            requestCurrent = requestCurrent.Replace("APIKEY", API.Key);
+            showJSON(requestCurrent);
+        }
+        private void showJSON(string request)
+        {
+            HttpClient client = new HttpClient();
+            var response = client.GetStringAsync(request).Result;
+            var jsonObject = JsonNode.Parse(response);
+            output.Text = jsonObject.ToString();
         }
     }
 }
